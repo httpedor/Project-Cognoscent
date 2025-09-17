aae6b61c1ace2dac638bfcfdcc4b50536c3c3a94
httpedor <httpedor.dev@gmail.com>
2025-09-17 01:37:34 -0300

    more stuff

---
+++ Rpg/Skills/Animation/SkillAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Rpg.Animation;
+
+public class SkillAnimation
+{
+    protected SkillData _skillData;
+    protected SkillAnimation(SkillData skillData)
+    {
+        _skillData = skillData;
+    }
+    protected SkillAnimation(SkillData skillData, JsonObject json) : this(skillData)
+    {
+        
+    }
+    public static SkillAnimation? FromJson(SkillData skill, JsonObject json)
+    {
+        if (!json.ContainsKey("type"))
+            return null;
+
+        return json["type"]!.ToString() switch
+        {
+            "melee" => new MeleeAnimation(skill, json),
+            _ => null
+        };
+    }
+}
